1a86e66508609eda27f00cacf068e2c76f9fb457
MostafaPro0 <MostafaPro0@yahoo.com>
2024-05-17 14:23:10 +0300

    asd

---
+++ MangaRestaurant.Repository/Data/Migrations/20240313204030_InitialCreate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MangaRestaurant.Repository.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryMethodId",
+                table: "DeliveryMethods");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DeliveryMethodId",
+                table: "DeliveryMethods",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
